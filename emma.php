6ab65408135e0c2df3ff83bac03b837e6d9b96ac
EmmaDruot <48950076+EmmaDruot@users.noreply.github.com>
2020-09-03 16:23:11 +0200

    Mon projet perso

--- emma.php
+++ emma.php
@@ -1,5 +1,4 @@
 <?php
-echo '<h1>Ceci est le fichier de Emma</h1>';
-echo '<h1>Eeeee-cchooooo</h1>';
-echo '<h2>Ma deuxieme phrase -Emma-</h2>';
+echo '<h1>Bonjour, ce fichier est à Emma !</h1>';
+
 
